c6bca0bfb86292d624906b7053eb6b09196dcdb8
Benoit Souillat <tido.squat@gmail.com>
2023-07-10 15:35:05 +0200

    prepare login rules

--- index.php
+++ index.php
@@ -20,24 +20,28 @@
             $stmt = $conn->query($all_items);
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) :
             ?>
-                <div class="card">
-                    <p class="card_name"><?php echo htmlspecialchars($row['name']); ?></p>
-                    <img class="rounded" src=<?php echo '.' . htmlspecialchars($row['img_path']); ?>>
-                    <p class="card_price"><?php echo htmlspecialchars($row['price'] . " €"); ?></p>
-                    <p class="card_desc"><?php echo htmlspecialchars($row['description']); ?></p>
-                    <div class="btn_container">
-                        <a type="button" class="card_btn_modify btn btn-warning" id="<?php echo $row['id']; ?>" href="./php/modify-item.php?id=<?php echo $row['id']; ?>">Modifier
-                        </a>
-                        <a type="button" class="card_btn_delete btn btn-danger" id="<?php echo $row['id']; ?>" href="./php/delete-item.php?id=<?php echo $row['id']; ?>">Suppression
-                        </a>
-                    </div>
+            <div class="card">
+                <p class="card_name"><?php echo htmlspecialchars($row['name']); ?></p>
+                <img class="rounded" src=<?php echo '.' . htmlspecialchars($row['img_path']); ?>>
+                <p class="card_price"><?php echo htmlspecialchars($row['price'] . " €"); ?></p>
+                <p class="card_desc"><?php echo htmlspecialchars($row['description']); ?></p>
+                <div class="btn_container">
+                    <a type="button" class="card_btn_modify btn btn-warning" id="<?php echo $row['id']; ?>"
+                        href="./php/modify-item.php?id=<?php echo $row['id']; ?>">Modifier
+                    </a>
+                    <a type="button" class="card_btn_delete btn btn-danger" id="<?php echo $row['id']; ?>"
+                        href="./php/delete-item.php?id=<?php echo $row['id']; ?>">Suppression
+                    </a>
                 </div>
+            </div>
             <?php endwhile; ?>
         </div>
         <div class="action_container">
             <a href="./php/form_add_item.php" class="btn btn-primary">Ajouter un article</a>
             <a href="#" class="btn btn-info">Rechercher</a>
             <a href="./php/register.php" class="btn btn-success">S'inscrire</a>
+            <!-- Conditionner l'affichage des boutons Se connecter, S'inscrire, Se deconnecter , Ajouter un article en fonction de l'état de connexion de l'utilisateur -- utiliser écho après vérification -->
+            <a href="./php/login.php" class="btn btn-success">Se connecter</a>
         </div>
     </section>
 
--- php/login.php
+++ php/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>S'inscrire</title>
+    <title>Se Connecter</title>
     <?php
     require_once(__DIR__ . '/../src/headlinks.php');
     ?>
@@ -12,7 +12,7 @@
 
 <body class="d-flex flex-column justify-content-center align-items-center">
     <h1>Espace utilisateur</h1>
-    <h2> S'inscrire sur notre site </h2>
+    <h2> Se connecter sur notre site </h2>
     <section class="container-fluid register_bloc w-100 d-flex justify-content-center">
         <form id="form_add_user" method="post" action="./users/register_user.php" enctype="multipart/form-data">
             <p class="error">
@@ -23,33 +23,12 @@
                 ?>
             </p>
             <div class="form-group p-2 m-2">
-                <label for="firstname">Prénom : </label>
-                <input class="form-control" type="text" name="firstname" id="firstname" required>
-            </div>
-            <div class="form-group p-2 m-2">
-                <label for="lastname">Nom : </label>
-                <input class="form-control" type="text" name="lastname" id="lastname" required>
-            </div>
-            <div class="form-group p-2 m-2">
                 <label for="email">Email : </label>
                 <input class="form-control" type="email" name="email" id="email" required>
             </div>
             <div class="form-group p-2 m-2">
-                <label for="email-verification">Vérifier Email : </label>
-                <input class="form-control" type="email" name="email-verification" id="email-verification" required>
-            </div>
-            <div class="form-group p-2 m-2">
                 <label for="password">Mot de passe : </label>
                 <input class="form-control" type="password" name="password" id="password" required>
-            </div>
-            <div class="form-group p-2 m-2">
-                <label for="password-verification">Vérifiez le Mot de passe : </label>
-                <input class="form-control" type="password" name="password-verification" id="password-verification" required>
-            </div>
-            <div class="form-group p-2 m-2">
-                <input type="hidden" name="MAX_FILE_SIZE" value="5000000">
-                <label for="profil_img">Choisissez une image de profil</label>
-                <input class="form-control" type="file" name="profil_img" id="profil_img">
             </div>
             <p id="error" class="error"></p>
             <div class="d-flex flex-row justify-content-around mt-4">
@@ -59,7 +38,7 @@
         </form>
         <div class="action_container">
             <a href="../index.php" class="btn btn-info">Retour à l'accueil</a>
-            <a href="./login.php" class="btn btn-primary">Se connecter</a>
+            <a href="./register.php" class="btn btn-primary">S'inscrire</a>
         </div>
     </section>
 
