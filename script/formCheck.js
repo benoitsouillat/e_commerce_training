9c265178ba919e507deca1a83623f53a882c95b4
Benoit Souillat <tido.squat@gmail.com>
2023-07-10 13:28:17 +0200

    adding verification on modify formulaire and check

--- script/formCheck.js
+++ script/formCheck.js
@@ -1,43 +1,42 @@
 
 formAddItem = document.getElementById("form_add_item");
+formModifyItem = document.getElementById("form_modify_item");
 formAddUser = document.getElementById("form_add_user");
 
 regStr = new RegExp('[A-z]');
 regNum = new RegExp('[0-9]');
 
-if (formAddItem) {
-    formAddItem.addEventListener("submit", (e) => {
-
-        articleName = document.getElementById("name").value;
-        articlePrice = document.getElementById("price").value;
-        articleDescription = document.getElementById("description").value;
-        error = "";
-
-        if (articleDescription.length <= 10) {
-            error = "La description doit être plus longue pour définir l'article";
-        }
-        if (!regNum.test(articlePrice)) {
-            error = "Merci d'écrire uniquement des chiffres pour définir le prix";
-        }
-        if (!articleDescription) {
-            error = "Vous devez définir une description de l'article";
-        }
-        if (!articlePrice) {
-            error = "Vous devez définir un prix";
-        }
-
-        if (articleName.length <= 4) {
-            error = "Le nom de l'article est trop petit";
-        }
-        if (!regStr.test(articleName)) {
-            error = "Merci d'utiliser que des caractères normaux";
-        }
-        if (error != "") {
-            e.preventDefault();
-            document.getElementById('error').innerHTML = error;
-
-        }
-
+if (formAddItem || formModifyItem) {
+    form = Array.from(document.getElementsByTagName('form'));
+    form.forEach(element => {
+        element.addEventListener("submit", (e) => {
+            articleName = document.getElementById("name").value;
+            articlePrice = document.getElementById("price").value;
+            articleDescription = document.getElementById("description").value;
+            error = "";
+            if (articleDescription.length <= 10) {
+                error = "La description doit être plus longue pour définir l'article";
+            }
+            if (!regNum.test(articlePrice)) {
+                error = "Merci d'écrire uniquement des chiffres pour définir le prix";
+            }
+            if (!articleDescription) {
+                error = "Vous devez définir une description de l'article";
+            }
+            if (!articlePrice) {
+                error = "Vous devez définir un prix";
+            }
+            if (!regStr.test(articleName)) {
+                error = "Merci d'utiliser que des caractères normaux";
+            }
+            if (articleName.length <= 4) {
+                error = "Le nom de l'article est trop petit";
+            }
+            if (error != "") {
+                e.preventDefault();
+                document.getElementById('error').innerHTML = error;
+            }
+        });
     });
 }
 if (formAddUser) {
