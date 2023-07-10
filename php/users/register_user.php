dfc8e3c65b1423ebe1c2f3e13c36df3c0ffc6660
Benoit Souillat <tido.squat@gmail.com>
2023-07-10 10:41:53 +0200

    create register form - working on DB

--- php/users/register_user.php
+++ php/users/register_user.php
@@ -5,6 +5,7 @@ const DEFAULT_PROFIL_PATH = "/media/users/default.jpg";
 require_once(__DIR__ . '/../components/connection.php');
 require_once(__DIR__ . '/../components/sql.php');
 require_once(__DIR__ . '/../components/format_str.php');
+require_once(__DIR__ . '/../components/items_functions.php');
 
 if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
 
@@ -14,7 +15,9 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
     $img_profil_path = DEFAULT_PROFIL_PATH;
 
     if (isset($_FILES['profil_img'])) {
-        $file_name = replace_space(strtolower($firstname . '-' . $lastname)) . '.jpg';
+        $id = get_last_id_number($conn, $all_users);
+
+        $file_name = $id + 1 . replace_space(strtolower($firstname . '-' . $lastname)) . '.jpg';
         $file_tmp = $_FILES['profil_img']['tmp_name'];
         $file_destination = '../../media/users/' . $file_name;
 
